df5f7aa50b81cef4a8af087ea71439d6024b99c1
Hong-Yi Wang <wanghy2007@gmail.com>
2019-03-01 18:18:29 -0800

    Refactor api.py.

--- app/api.py
+++ app/api.py
@@ -24,15 +24,6 @@ def start_response(color):
         })
     )
 
-def _create_board(height, width, value):
-    return [[value for x in range(width)] for y in range(height)]
-
-def _populate_food(board, food):
-    for coord in food:
-        x = coord['x']
-        y = coord['y']
-        board[y][x] = FOOD
-
 def _extract_enemy_you(snakes, you):
     enemy_snakes = []
     for snake in snakes:
@@ -40,7 +31,17 @@ def _extract_enemy_you(snakes, you):
             enemy_snakes.append(snake)
     return [enemy_snakes, you]
 
-def _populate_snakes(board, enemy_snakes, you_snake):
+def _create_board(height, width, food, enemy_snakes, you_snake):
+    # create board
+    board = [[EMPTY for x in range(width)] for y in range(height)]
+
+    # populate food
+    for coord in food:
+        x = coord['x']
+        y = coord['y']
+        board[y][x] = FOOD
+
+    # populate enemy snakes
     for i in range(len(enemy_snakes)):
         snake_value = i+2
         snake_body = enemy_snakes[i]['body']
@@ -49,11 +50,14 @@ def _populate_snakes(board, enemy_snakes, you_snake):
             y = coord['y']
             board[y][x] = snake_value
 
+    # populate you snake
     snake_body = you_snake['body']
     for coord in snake_body:
         x = coord['x']
         y = coord['y']
         board[y][x] = YOU
+
+    return board
 
 def _calculate_distance(x, y, enemy_snake_heads):
     distance_list = [abs(x-head['x'])+abs(y-head['y']) for head in enemy_snake_heads]
@@ -89,16 +93,12 @@ def _print_board(board):
 def move_process(data):
     height = data['board']['height']
     width = data['board']['width']
-    board = _create_board(height, width, EMPTY)
-
     food = data['board']['food']
-    _populate_food(board, food)
-    snakes = data['board']['snakes']
-    you = data['you']
-    enemy_snakes, you_snake = _extract_enemy_you(snakes, you)
-    print('enemy: '+str(enemy_snakes))
-    print('you: '+str(you_snake))
-    _populate_snakes(board, enemy_snakes, you_snake)
+    enemy_snakes, you_snake = _extract_enemy_you(
+        data['board']['snakes'],
+        data['you']
+    )
+    board = _create_board(height, width, food, enemy_snakes, you_snake)
 
     distance_matrix = _create_distance_matrix(height, width, enemy_snakes)
     _print_board(board)
